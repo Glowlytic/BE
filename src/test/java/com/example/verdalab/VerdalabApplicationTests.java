99ba8afc8089eaecc4986f7822cd7cf4b2407f1a
luantnk <nkluantran2907@gmail.com>
2025-12-28 14:21:36 +0700

    first commit

---
+++ src/test/java/com/example/verdalab/VerdalabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.verdalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdalabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
