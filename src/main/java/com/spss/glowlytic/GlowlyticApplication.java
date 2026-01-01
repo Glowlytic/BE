5c647931150a8dbf85993bf153c8916516dc7960
luantnk <nkluantran2907@gmail.com>
2026-01-01 16:02:33 +0700

    new project

---
+++ src/main/java/com/spss/glowlytic/GlowlyticApplication.java
@@ -0,0 +1,13 @@
+package com.spss.glowlytic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowlyticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlowlyticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spss/glowlytic/GlowlyticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spss.glowlytic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowlyticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
